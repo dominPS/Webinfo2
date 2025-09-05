3583964b839734a0cc7523494edbea8c49646886
Dominik Kozak <dominik.kozak@polsystem.pl>
2025-09-05 22:19:33 +0200

    some refactoring and styling improvements

--- src/pages/ETeczka/ETeczkaPage.tsx
+++ src/pages/ETeczka/ETeczkaPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { useTheme, Theme } from '@emotion/react';
 import { useTranslation } from 'react-i18next';
 
 const Container = styled.div`
@@ -10,6 +11,14 @@ const Title = styled.h1`
   font-size: 24px;
   margin-bottom: 24px;
 `;
+declare module '@emotion/react' {
+  export interface Theme {
+    shadows: {
+      large: string;
+      [key: string]: string;
+    };
+  }
+}
 
 const Card = styled.div`
   background-color: white;
